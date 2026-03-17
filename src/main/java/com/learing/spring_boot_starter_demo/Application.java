67ec0a1dd0c5ae14473641cb598e698127302d8e
conqdat <trancongdat.datct@gmail.com>
2026-03-17 23:16:15 +0700

    feat: init project

---
+++ src/main/java/com/learing/spring_boot_starter_demo/Application.java
@@ -0,0 +1,13 @@
+package com.learing.spring_boot_starter_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learing/spring_boot_starter_demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learing.spring_boot_starter_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
